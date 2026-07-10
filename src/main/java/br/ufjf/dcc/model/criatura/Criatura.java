c2a662a0ac3faf9c30625156245b62cb2cb71388
Marcela <marcela.sdeo4@gmail.com>
2026-07-10 18:47:12 -0300

    cria classe abstrata Criatura

---
+++ src/main/java/br/ufjf/dcc/model/criatura/Criatura.java
@@ -0,0 +1,15 @@
+package br.ufjf.dcc.model.criatura;
+
+public abstract class Criatura {
+
+    protected String nome;
+    protected int idade;
+    protected int nivel;
+    protected int experiencia;
+    protected int energia;
+    protected int saciedade;
+    protected int felicidade;
+    protected int saude;
+    protected boolean vivo;
+
+}
