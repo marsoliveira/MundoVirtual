449bdca00b1fb93250d4e37dc74f1198b5d049b5
Marcela <marcela.sdeo4@gmail.com>
2026-07-11 16:05:52 -0300

    altera enumerador TipoAlimento para incluir saciedade do alimento

--- src/main/java/br/ufjf/dcc/model/enums/TipoAlimento.java
+++ src/main/java/br/ufjf/dcc/model/enums/TipoAlimento.java
@@ -2,21 +2,38 @@ package br.ufjf.dcc.model.enums;
 
 public enum TipoAlimento {
 
-    FRUTA("Fruta"),
-    CARNE("Carne"),
-    FOTONS("Fotons"),
-    NECTAR_LUMINOSO("Nectar Luminoso"),
-    COGUMELOS("Cogumelos"),
-    CRISTAIS_ENERGETICOS("Cristais Energeticos"),
-    BANQUETE_REAL("Feliz");
+    FRUTA("Fruta", 20),
+    CARNE("Carne", 35),
+    FOTONS("Fotons", 20),
+    NECTAR_LUMINOSO("Nectar Luminoso", 35),
+    COGUMELOS("Cogumelos", 20),
+    CRISTAIS_ENERGETICOS("Cristais Energeticos", 35),
+    BANQUETE_REAL("Banquete Real", 50);
 
     private final String descricao;
+    private final int saciedade;
 
-    TipoAlimento(String descricao) {
+    TipoAlimento(String descricao, int saciedade) {
         this.descricao = descricao;
+        this.saciedade = saciedade;
     }
 
     public String getDescricao() {
         return descricao;
+    }
+
+    public int getSaciedade() {
+        return saciedade;
+    }
+
+    public static TipoAlimento obterPorDescricao(String descricao) {
+
+        for (TipoAlimento tipo : TipoAlimento.values()) {
+            if (tipo.getDescricao().equalsIgnoreCase(descricao)) {
+                return tipo;
+            }
+        }
+
+        throw new IllegalArgumentException("Tipo de alimento inválido: " + descricao);
     }
 }
