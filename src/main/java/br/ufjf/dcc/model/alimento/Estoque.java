843d191a38fbb5d3e01708149d9ff53793963578
Marcela <marcela.sdeo4@gmail.com>
2026-07-11 16:09:54 -0300

    refatora classe Estoque para encapsular lista de alimentos e implementar métodos de busca e consumo

--- src/main/java/br/ufjf/dcc/model/alimento/Estoque.java
+++ src/main/java/br/ufjf/dcc/model/alimento/Estoque.java
@@ -3,12 +3,45 @@ package br.ufjf.dcc.model.alimento;
 import java.io.IOException;
 import java.util.List;
 
+import br.ufjf.dcc.model.enums.TipoAlimento;
 import br.ufjf.dcc.util.LeitorCSV;
 
 public class Estoque {
 
-    public static List<Alimento> carregarAlimentos(String caminho) throws IOException {
+    private List<Alimento> alimentos;
 
-        return LeitorCSV.carregar(caminho, new ConversorAlimentoCSV());
+    public Estoque(String caminho) throws IOException {
+        this.alimentos = LeitorCSV.carregar(caminho, new ConversorAlimentoCSV());
     }
+
+    public List<Alimento> getAlimentos() {
+        return alimentos;
+    }
+
+    public Alimento buscarAlimento(TipoAlimento tipo) {
+
+        for (Alimento alimento : alimentos) {
+
+            if (alimento.getTipo() == tipo) {
+                return alimento;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean possui(TipoAlimento tipoAlimento) {
+
+        Alimento alimento = buscarAlimento(tipoAlimento);
+
+        return alimento != null && alimento.getQuantidade() > 0;
+    }
+
+    public void consumir(TipoAlimento tipoAlimento) {
+
+        Alimento alimento = buscarAlimento(tipoAlimento);
+
+        alimento.consumir(1);
+    }
+
 }
