71ebe272d09e13324a13eca151041db6db91149a
Marcela <marcela.sdeo4@gmail.com>
2026-07-11 17:14:06 -0300

    implementa métodos de atividades e habilidade especial para a classe Lumini

--- src/main/java/br/ufjf/dcc/model/criatura/Lumini.java
+++ src/main/java/br/ufjf/dcc/model/criatura/Lumini.java
@@ -1,5 +1,82 @@
 package br.ufjf.dcc.model.criatura;
 
-public class Lumini {
+import br.ufjf.dcc.interfaces.Luminescente;
+import br.ufjf.dcc.model.enums.Atividade;
+import br.ufjf.dcc.model.enums.TipoAlimento;
+
+public class Lumini extends Criatura implements Luminescente {
+
+    public Lumini(String nome, int idade, int nivel, int experiencia, int energia, int saciedade, int felicidade) {
+        super(nome, "Lumini", idade, nivel, experiencia, energia, saciedade, felicidade);
+    }
+
+    @Override
+    public void treinar() {
+        if (this.sePodeTreinar()) {
+            this.experiencia += 25;
+            this.energia -= 20;
+            this.saciedade -= 10;
+			this.ultimaAtividade = Atividade.TREINAR;
+        }
+    }
+
+	@Override
+	public void explorar() {
+        if (this.sePodeExplorar()) {
+            this.experiencia += 15;
+            this.energia -= 20;
+            this.saciedade -= 5;
+            this.felicidade += 10;
+			this.ultimaAtividade = Atividade.EXPLORAR;
+        }
+    }
+
+	@Override
+	public void brincar() {
+        if (this.sePodeBrincar()) {
+			this.felicidade += 35;
+            this.energia -= 5;
+            this.saciedade -= 5;
+			this.ultimaAtividade = Atividade.BRINCAR;
+        }
+    }
+
+	@Override
+	public boolean aceitaAlimento(TipoAlimento tipoAlimento) {
+		return tipoAlimento == TipoAlimento.NECTAR_LUMINOSO || tipoAlimento == TipoAlimento.FOTONS || tipoAlimento == TipoAlimento.BANQUETE_REAL;
+    }
+
+	@Override
+	public void descansar() {
+        if (this.sePodeDescansar()) {
+            this.energia += 40;
+			this.ultimaAtividade = Atividade.DESCANSAR;
+        }
+    }
+
+	@Override
+	public void participarDesafio() {
+        if (this.sePodeParticiparDesafio()) {
+            this.experiencia += 45;
+            this.energia -= 35;
+            this.saciedade -= 15;
+            this.felicidade += 15;
+			this.ultimaAtividade = Atividade.PARTICIPAR_DESAFIO;
+        }
+    }
+
+	@Override
+	public void evoluir() {
+        if (this.sePodeEvoluir()) {
+            this.energia += 2;
+			this.ultimaAtividade = Atividade.EVOLUIR;
+        }
+    }
+
+	@Override
+	public void emitirLuz() {
+		this.felicidade += 10;
+		System.out.println(this.nome + " emitiu luz e sua felicidade agora é " + this.felicidade);
+	}
 
 }
