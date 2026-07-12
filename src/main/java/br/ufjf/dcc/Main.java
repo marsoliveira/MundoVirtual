89ce5cd1d239f13d24b9c932b81571b31cd2ea43
Marcela <marcela.sdeo4@gmail.com>
2026-07-12 16:30:41 -0300

    inicia Sistema na classe Main e instancia ListaCriaturas e Estoque a partir de arquivos CSV e JSON

--- src/main/java/br/ufjf/dcc/Main.java
+++ src/main/java/br/ufjf/dcc/Main.java
@@ -1,16 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package br.ufjf.dcc;
 
-/**
- *
- * @author marce
- */
+import java.io.IOException;
+
+import br.ufjf.dcc.controller.Sistema;
+import br.ufjf.dcc.model.alimento.Estoque;
+import br.ufjf.dcc.model.criatura.ListaCriaturas;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+
+        ListaCriaturas listaCriaturas = new ListaCriaturas("src/main/resources/criaturas.json");
+
+        Estoque estoque = new Estoque("src/main/resources/alimentos.csv");
+
+        Sistema sistema = new Sistema(listaCriaturas, estoque);
+
+        sistema.iniciar();
     }
 }
